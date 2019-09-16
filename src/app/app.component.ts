b324fc23a23097c6fde95e60334bc8834051cb2a
gugutz <gugutz@gmail.com>
2019-09-16 07:17:19 -0300

    missing files from last commits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import data from "../../data/db.json";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'stairs-frontend';
+  title = "stairs-frontend";
+
+  constructor() {
+    const guests = data.guests;
+    if (window.localStorage.getItem("guests") === null) {
+      window.localStorage.setItem("guests", JSON.stringify(guests));
+    }
+  }
 }
