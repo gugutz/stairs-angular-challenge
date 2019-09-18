773b0c4babdf4cbf7fcd60001e0982fbf9a519db
gugutz <gugutz@gmail.com>
2019-09-18 13:14:40 -0300

    moved template to html file and use GuestStore class for data

--- src/app/list-all-guests/list-all-guests.component.ts
+++ src/app/list-all-guests/list-all-guests.component.ts
@@ -1,70 +1,29 @@
 import { Component, OnInit } from "@angular/core";
+import { GuestStore } from "../guest-store";
+import { GuestService } from "../guest.service";
 
 @Component({
   selector: "app-list-all-guests",
   templateUrl: "./list-all-guests.component.html",
-  styleUrls: ["./list-all-guests.component.css"],
-  template: `
-    <div class="container">
-      <table class="table">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="col">Nome</th>
-            <th scope="col">Email</th>
-            <th scope="col">Endereço</th>
-            <th scope="col">Telefone</th>
-            <th scope="col">Detalhar | Editar | Excluir</th>
-          </tr>
-        </thead>
-        <tbody>
-          <ng-container>
-            <tr *ngFor="let guest of guests">
-              <td>{{ guest.name }}</td>
-              <td>{{ guest.email }}</td>
-              <td>{{ guest.address }}</td>
-              <td>{{ guest.phone }}</td>
-              <td>
-                <a
-                  class="nav-link float-left"
-                  href="#"
-                  routerLink="/guest/{{guest.id}}"
-                  routerLinkActive="active"
-                >
-                  <button (click)="guestDetails(guest.id)">Detalhes</button>
-                </a>
-                <a
-                  class="nav-link float-left"
-                  href="#"
-                  routerLink="/guest/{{guest.id}}/edit"
-                  routerLinkActive="active"
-                >
-                  <button>Editar</button>
-                </a>
-                <a
-                  class="nav-link"
-                  href="#"
-                  routerLink="/guest/{{guest.id}}/delete"
-                  routerLinkActive="active"
-                >
-                  <button>Excluir</button>
-                </a>
-              </td>
-            </tr>
-          </ng-container>
-        </tbody>
-      </table>
-    </div>
-  `
+  styleUrls: ["./list-all-guests.component.css"]
 })
 export class ListAllGuestsComponent implements OnInit {
-  titulo = "teste";
-  data: string;
-  guests: Array<Object>;
-  constructor() {
-    this.data = localStorage.getItem("guests");
-    this.guests = JSON.parse(this.data);
-    console.log(this.guests);
+  guests: Object;
+  guestService;
+  error;
+
+  constructor(guestService: GuestService) {
+    const guestStore = new GuestStore();
+    this.guests = guestStore.getGuests();
+    this.guestService = guestService;
   }
 
+  exportGuests() {
+    console.log(JSON.stringify(this.guests));
+    this.guestService.sendGuestsToApi(this.guests).subscribe(
+      (guests: Object) => (this.guests = { ...guests }), // success path
+      error => (this.error = error) // error path
+    );
+  }
   ngOnInit() {}
 }
