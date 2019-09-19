36f13d4cbab5fef0bbc073d44e022fd95ebdc19b
gugutz <gugutz@gmail.com>
2019-09-19 17:38:04 -0300

    removed delete component and turned it into actions inside other controllers

--- src/app/list-all-guests/list-all-guests.component.ts
+++ src/app/list-all-guests/list-all-guests.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
 import { GuestStore } from "../guest-store";
 import { GuestService } from "../guest.service";
+import { Location } from "@angular/common";
 
 @Component({
   selector: "app-list-all-guests",
   templateUrl: "./list-all-guests.component.html",
   styleUrls: ["./list-all-guests.component.css"]
 })
-export class ListAllGuestsComponent implements OnInit {
+export class ListAllGuestsComponent {
   guests: Object;
-  guestService;
   error;
 
-  constructor(guestService: GuestService) {
+  constructor(
+    guestService: GuestService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {
     const guestStore = new GuestStore();
-    this.guests = guestStore.getGuests();
+    this.guests = guestStore.list();
     this.guestService = guestService;
+  }
+
+  onDelete(id: string) {
+    const guestStore = new GuestStore();
+    console.log("inside delete guest id " + id);
+    guestStore.delete(id);
+    this.guests = guestStore.list();
   }
 
   exportGuests() {
@@ -25,5 +38,4 @@ export class ListAllGuestsComponent implements OnInit {
       error => (this.error = error) // error path
     );
   }
-  ngOnInit() {}
 }
